88f1218ed24d7bb04aff908e4fef1380ad99fe1c
Niquinhoo <nicotrib699@gmail.com>
2026-04-07 22:26:28 -0300

    cart fix 1{

--- routes/cart.router.js
+++ routes/cart.router.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const { productos } = require('../data/db');
+
 router.get('/', (req, res) => {
-    res.render('pages/cart-page');
+    // Importamos productos de la DB y les agregamos cantidad
+    const cartItems = [
+        { ...productos[0], quantity: 2 }, // Burger Smash XL (1200) * 2 = 2400
+        { ...productos[1], quantity: 1 }  // Pizza Napolitana (1500) * 1 = 1500
+    ];
+    
+    const cartTotal = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+    
+    res.render('pages/cart/cart-page', { cartItems, cartTotal });
 });
 
 module.exports = router;
